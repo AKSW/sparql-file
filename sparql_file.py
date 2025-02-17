9b970e01da82945151177133f8a798f9e84419cb
Natanael Arndt <arndtn@gmail.com>
2025-02-17 11:38:33 +0100

    Add EXAMPLE_QUERY

--- sparql_file.py
+++ sparql_file.py
@@ -1,8 +1,13 @@
 from rdflib import Graph
 from rdflib_endpoint import SparqlEndpoint
+from textwrap import dedent
+
 import os
 
 GRAPH_FILE = os.getenv("GRAPH_FILE")
+EXAMPLE_QUERY = os.getenv("EXAMPLE_QUERY", dedent("""
+select * { ?s ?p ?o } limit 10
+""").strip())
 
 g = Graph()
 
@@ -12,4 +17,5 @@ with open(GRAPH_FILE, "r") as graph_file_io:
 # Start the SPARQL endpoint based on the RDFLib Graph
 app = SparqlEndpoint(
     graph=g,
+    example_query=EXAMPLE_QUERY,
 )
