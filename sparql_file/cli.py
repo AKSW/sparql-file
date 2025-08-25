1b191a74d1638e5b4f464df395a60b81ea055c7d
Natanael Arndt <arndtn@gmail.com>
2025-08-25 16:41:19 +0200

    bind to localhost per default and add info about the host

--- sparql_file/cli.py
+++ sparql_file/cli.py
@@ -9,12 +9,16 @@ app = typer.Typer()
 @app.command()
 def cli(
     graph_file: str,
-    host: str = "0.0.0.0",
+    host: str = "localhost",
     port: int = 8000,
     example_query: str = DEFAULT_EXAMPLE_QUERY,
     graph_format: str | None = None,
 ):
-    """Start a SPARQL 1.1 endpoint based on the given RDF file."""
+    """Start a SPARQL 1.1 endpoint based on the given RDF file.
+
+    If you want to bind to every address on the IPv4 and IPv6 stack set --host ''
+    (cf. https://chaos.social/@white_gecko/114184354432052312)
+    """
     endpoint = sparql_file(graph_file, example_query)
     uvicorn.run(endpoint, host=host, port=port)
 
