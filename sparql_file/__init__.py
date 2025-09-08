0bf11624af112b5a43f8e7ed05744e997033df6d
Natanael Arndt <arndtn@gmail.com>
2025-09-08 11:09:05 +0200

    Name it Dataset

--- sparql_file/__init__.py
+++ sparql_file/__init__.py
@@ -1,4 +1,4 @@
-from rdflib import Dataset as Graph
+from rdflib import Dataset
 from rdflib_endpoint import SparqlEndpoint
 
 DEFAULT_EXAMPLE_QUERY = "select * { ?s ?p ?o } limit 10"
@@ -9,16 +9,16 @@ def sparql_file(
     example_query: str | None = None,
     graph_format: str | None = None,
 ):
-    g = Graph()
+    ds = Dataset()
 
     if example_query is None:
         example_query = DEFAULT_EXAMPLE_QUERY
 
     with open(graph_file, "r") as graph_file_io:
-        g.parse(source=graph_file_io, format=graph_format)
+        ds.parse(source=graph_file_io, format=graph_format)
 
     # Return the SPARQL endpoint based on the RDFLib Graph
     return SparqlEndpoint(
-        graph=g,
+        graph=ds,
         example_query=example_query,
     )
